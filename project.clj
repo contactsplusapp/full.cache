d509d4178ea6289bab19b9629e7274df93c39d71
Mor Gazith <mor.gazith@benchmarkemail.com>
2023-03-02 15:52:18 +0200

    CON-1564 / Remove ahead-of-time flag from all in-use FullContact libraries

--- project.clj
+++ project.clj
@@ -1,18 +1,21 @@
-(defproject fullcontact/full.cache "1.2.1"
+(defproject fullcontact/full.cache "1.2.2"
   :description "In-memory + memcache caching for Clojure with async loading."
-  :url "https://github.com/fullcontact/full.cache"
+  :url "https://github.com/contactsplusapp/full.cache"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo}
-  :deploy-repositories [["releases" {:url "https://clojars.org/repo/" :creds :gpg}]]
+  :repositories [["fullcontact" {:url "https://contactsplus.jfrog.io/artifactory/repo"}]
+                 ["releases" {:url "https://contactsplus.jfrog.io/artifactory/libs-release-local"}]
+                 ["snapshots" {:url "https://contactsplus.jfrog.io/artifactory/libs-snapshot-local"}]]
+  :deploy-repositories [["releases" {:url "https://contactsplus.jfrog.io/artifactory/libs-release-local" :sign-releases false}]
+                        ["snapshots" {:url "https://contactsplus.jfrog.io/artifactory/libs-snapshot-local" :sign-releases false}]]
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [net.jodah/expiringmap "0.4.1"]
                  [net.spy/spymemcached "2.12.2"]
                  [com.taoensso/nippy "2.14.0"]
-                 [fullcontact/full.core "1.1.1"
+                 [fullcontact/full.core "1.1.3"
                   :exclusions [org.clojure/clojurescript]]
-                 [fullcontact/full.async "1.1.0"]]
-  :aot :all
+                 [fullcontact/full.async "1.1.1"]]
   :plugins [[lein-midje "3.2.2"]]
   :release-tasks [["vcs" "assert-committed"]
                   ["change" "version" "leiningen.release/bump-version" "release"]
